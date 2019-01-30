a7b1c54dc986ba057c686069f3dcfa505a3ecbb8
Sindorman <mikebykhovtsev@gmail.com>
2019-01-29 22:30:32 -0800

    Finished, just need tweet hashtag

--- python/cpts215/PA1/twitter_sort.py
+++ python/cpts215/PA1/twitter_sort.py
@@ -6,24 +6,31 @@ class record:
 
     def __init__(self, string):
         string = string.split('\"')
-        self.name = str(string[0])[1:len(string[0])]
-        self.message = string[1]
-        self.datetime_object = datetime.strptime(string[2], '%Y %d %M %H:%M:%S')
-    
+        self.name = str(string[0])[1:(len(string[0])-1)]
+        self.message = string[1][0:(len(string[1])-1)]
+        self.datetime_object = datetime.strptime(str(string[2][0:(len(string[2])-1)]), ' %Y %m %d %H:%M:%S')
+        self.char_length = len(string[1][0:(len(string[1])-1)])
+
     def get_name(self):
         return self.name
 
     def get_message(self):
         return self.message
 
+    def get_char_length(self):
+        return self.char_length
+
     def get_datetime(self):
         return self.datetime_object
 
     def is_more_recent(self, record):
         return self.datetime_object < record.get_datetime()
-    
-    def __lt__(self, other):
+
+    def __gt__(self, other):
         return self.is_more_recent(other)
+    
+    def __str__(self):
+        return str("{} \"{}\" {}".format(self.name, self.message, str(self.datetime_object)))
 
 
 def read_records(filename):
@@ -38,6 +45,7 @@ def read_records(filename):
     while line != "":
         rec = record(line)
         record_list.append(rec)
+        line = s.readline()
 
     return record_list
 
@@ -57,6 +65,17 @@ def merge_and_sort_tweets(records_list1, records_list2):
     records_list1.extend(records_list2)
     return sorted(records_list1)
 
+def count_tweets_hashtags(records_list):
+    count_total = 0
+    count_short = 0
+    count_over = 0
+    for r in records_list:
+        count_total += r.get_char_length()
+        if(r.get_char_length() > 140):
+            count_over += 1
+        elif(r.get_char_length() < 50):
+            count_short += 1
+    print("Number of tweets over 140 character limit: {}\nNumber of short tweets: {}\nAverage character per tweet: {}".format(count_over, count_short, round(count_total/len(records_list))))
 
 def main():
     if len(sys.argv) < 3:
@@ -65,7 +84,9 @@ def main():
     records1 = read_records(sys.argv[1])
     records2 = read_records(sys.argv[2])
     sorted_rec = merge_and_sort_tweets(records1, records2)
-    print(sorted_rec)
+    for r in sorted_rec:
+        print(r)
+    count_tweets_hashtags(sorted_rec)
 
 if __name__ == "__main__":
     main()
