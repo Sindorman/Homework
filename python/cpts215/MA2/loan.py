dc46c8bba9529c07ced7fc08229b5371cfda5614
Sindorman <mikebykhovtsev@gmail.com>
2019-02-09 19:11:29 -0800

    Finished

--- python/cpts215/MA2/loan.py
+++ python/cpts215/MA2/loan.py
@@ -12,14 +12,14 @@ def balance(cost, n , r):
     r_m = r / 1200
     return round((cost * (1 + r_m)**n_m) - monthly_payment(cost, n, r)*(((1 + r_m)**n_m - 1)/r_m), 2)
 
-def calc_loan(cost, n, r):
+def calc_loan(name, cost, n, r):
     monthly_pay = monthly_payment(cost, n , r)
     print("Monthly payment: {}".format(monthly_pay))
 
     table = list()
     total_interest = 0
     balance = cost
-    while balance >= 0:
+    while balance > 0:
         interest = round(balance * (r / 1200), 2)
         total_interest += interest
         principle = round(monthly_pay - interest, 2)
@@ -35,40 +35,51 @@ def calc_loan(cost, n, r):
         print ("{:<7}${:>10.2f}   ${:>8.2f}   ${:>7.2f}   ${:>8.2f}".format(index, t[0], t[1], t[2], t[3]))
         index += 1
     print("{:<7}${:>10.2f}   ${:>8.2f}   ${:>7.2f}   ${:>8.2f}".format(index, 0.00, 0.00, 0.00, 0.00))
-    plot_graph(cost, table, n)
+    plot_graph(name, table, n)
 
-def plot_graph(balance, table, n):
-    balance_x = np.array(list(range(0, n*12)))
+def plot_graph(name, table, n):
+    balance_x = np.array(list(range(0, n + 1)))
     balance_y = list()
 
-    labels = ["Balance Remaining", "Principle Paid", "Interest Paid"]
+    labels = ["Principle Paid", "Interest Paid"]
 
     principle_paid = list()
-    principle = table[0][1]
-    interest = table[0][2]
     interest_paid = list()
-
-    for t in table:
-        balance_y.append(int(t[0]))
-        principle_paid.append(int(principle))
-        interest_paid.append(int(interest))
-        principle += t[1]
-        interest += t[2]
+    principle = 0
+    interest = 0
+    
+    index = 0
+    while index < len(table):
+        if index % 12 == 0:
+            balance_y.append(table[index][0])
+            principle_paid.append(int(principle))
+            interest_paid.append(int(interest))
+        principle += table[index][1]
+        interest += table[index][2]
+        index += 1
+    balance_y.append(0)
+    principle_paid.append(int(principle))
+    interest_paid.append(int(interest))
     
     balance_y = np.array(balance_y)
     principle_paid = np.array(principle_paid)
     interest_paid = np.array(interest_paid)
     #plt.rc('font', family='serif', size=13)
-    plt.stackplot(balance_x, balance_y, principle_paid, interest_paid,  labels=labels)
-    plt.legend(loc='upper left')
+    plt.stackplot(balance_x, principle_paid, interest_paid, colors=['g', "#FFA500"], labels=labels)
+    plt.title(name + " Loan")
+    balance_line = plt.plot(balance_x, balance_y, label="Balance Remaining")
+    plt.setp(balance_line, color='k', linewidth=2.0)
+    plt.legend(loc='upper center')
     plt.autoscale(enable=True, axis=u'both', tight=False)
-    plt.xlabel('Balance', fontsize=15)
-    plt.ylabel('Years', fontsize=15)
+    plt.ylabel('Balance', fontsize=15)
+    plt.xlabel('Years', fontsize=15)
+    plt.axis([0, n, 0, table[0][0] + table[0][0] * 0.2])
+    plt.grid()
     plt.show()
 
 
 def main():
-    input("What is the loan for? ")    
+    name = input("What is the loan for? ")    
     try:        
         total = round(float(input("Please enter the principal amount for the loan: ")), 2)
         rate = float(input("Please enter the yearly interest rate (as a percent) for the loan: "))
@@ -76,7 +87,7 @@ def main():
     except ValueError:
         print("Error! That is not an number!")
         exit(1)
-    calc_loan(total, years, rate)
+    calc_loan(name, total, years, rate)
     #print("Balance: {}".format(balance(total, years, rate)))
 
 if __name__ == "__main__":
