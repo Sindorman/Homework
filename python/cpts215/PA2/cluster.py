1d1a9eeede97ef734c24582ec949952a13d5d0d8
Sindorman <mikebykhovtsev@gmail.com>
2019-02-18 23:25:45 -0800

    Trying to cluster, need to finish

--- python/cpts215/PA2/cluster.py
+++ python/cpts215/PA2/cluster.py
@@ -41,15 +41,20 @@ class Cluster:
 
     def pop(self):
         return self.samples.pop()
+    
+    def empty(self):
+        self.samples = list()
 
     def compute_centroid(self):
+        if len(self.samples) is 0:
+            return
         self.centroid = list()
         x = 0
         while x < len(self.samples[0]):
             value = 0
             for s in self.samples:
                 value += s.genes[x]
-            self.centroid.append(round(value / len(self.samples), 5))
+            self.centroid.append(round(value / len(self.samples), 2))
             x += 1
 
     def get_centroid(self):
@@ -59,9 +64,14 @@ def main():
     if len(sys.argv) < 3:
         print("Error! Please run script providing name of the files containing data and number of clusters!")
         exit(1)
-    
+    try:
+        k = int(sys.argv[2])
+    except ValueError:
+        print("Error! That is not an number!")
+        exit(1)
     iris = pd.read_csv(sys.argv[1], delimiter = ',', index_col = 0, header=None)
-    
+    #plt.imshow(iris, cmap='seismic', interpolation='nearest')
+    #plt.show()
     i = 0
     samples = list()
     for index, row in iris.iterrows():
@@ -69,20 +79,60 @@ def main():
         samples.append(GeneSample(row.tolist()))
 
     clusters = list()
-    while i < sys.argv[2]:
+    temp_samples = list()
+
+    # Initializing empty clusters
+    while i < k:
         cl = Cluster()
         element = random.choice(samples)
         samples.remove(element)
         cl.append(element)
-        clusters.append()
+        clusters.append(cl)
+        temp_samples.append(element)
+        i += 1
+    samples.extend(temp_samples)
+    del temp_samples
 
-        
+    # Filling in the rest of samples
+    while len(samples) is not 0:
+        for cl in clusters:
+            cl = random.choice(clusters)
+            element = random.choice(samples)
+            samples.remove(element)
+            cl.append(element)
 
+    old_clusters = list()
+    done = False
+
+    # Whole process of clustering
+    while done is False:
+        old_clusters = clusters.copy()
+        for cl in clusters:
+            cl.compute_centroid()
+            cl.empty()
+
+        index = 0
+        distance = math.inf
+
+        # comparing to centroids and moving to the right cluster
+        for s in samples:
+            for cl in clusters:
+                d = s.dist(cl.get_centroid())
+                if d < distance:
+                    distance = d
+                    index = clusters.index(cl)
+            clusters[index].append(s)
+
+        # Checking if old clusters samples moved or not
+        index = 0
+        while index < len(old_clusters):
+            for s in old_clusters[index].samples:
+                if s in clusters[index].samples:
+                    done = True
+                else:
+                    done = False
+            index += 1
     #print(iris)
-
-    
-    #plt.imshow(iris, cmap='hot', interpolation='nearest')
-    #plt.show()
  
     
 if __name__ == "__main__":
