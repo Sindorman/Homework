a93c076376884ee585f57499c9b4245d0d99be2e
Sindorman <mikebykhovtsev@gmail.com>
2019-02-18 21:58:13 -0800

    Temp save. Working on clusters. Cluster and Gene sample class finished, heatmap working

--- python/cpts215/PA2/cluster.py
+++ python/cpts215/PA2/cluster.py
@@ -2,7 +2,8 @@
 
 import pandas as pd
 import math
-import matplotlib
+import matplotlib.pyplot as plt
+import random
 import sys
 
 class GeneSample:
@@ -14,6 +15,9 @@ class GeneSample:
     
     def __len__(self):
         return len(self.genes)
+
+    def __eq__(self, other):
+        return self.genes == other
 
     def dist(self, other):
 
@@ -30,17 +34,55 @@ class GeneSample:
 class Cluster:
     def __init__(self):
         self.samples = list()
+        self.centroid = list()
+
+    def append(self, sample):
+        self.samples.append(sample)
+
+    def pop(self):
+        return self.samples.pop()
+
+    def compute_centroid(self):
+        self.centroid = list()
+        x = 0
+        while x < len(self.samples[0]):
+            value = 0
+            for s in self.samples:
+                value += s.genes[x]
+            self.centroid.append(round(value / len(self.samples), 5))
+            x += 1
+
+    def get_centroid(self):
+        return self.centroid
 
 def main():
-    if len(sys.argv) < 2:
-        print("Error! Please run script providing name of the files containing data!")
+    if len(sys.argv) < 3:
+        print("Error! Please run script providing name of the files containing data and number of clusters!")
         exit(1)
     
-    iris = pd.read_csv(sys.argv[1], delimiter = ',', header=None)
-    #iris.info()
-    for thing in iris.iterrows():
-        print("This: {} is of type {}".format(thing, type(thing)))
-    print(iris)
+    iris = pd.read_csv(sys.argv[1], delimiter = ',', index_col = 0, header=None)
+    
+    i = 0
+    samples = list()
+    for index, row in iris.iterrows():
+        #print("Row {}, content: {}".format(index, row))
+        samples.append(GeneSample(row.tolist()))
+
+    clusters = list()
+    while i < sys.argv[2]:
+        cl = Cluster()
+        element = random.choice(samples)
+        samples.remove(element)
+        cl.append(element)
+        clusters.append()
+
+        
+
+    #print(iris)
+
+    
+    #plt.imshow(iris, cmap='hot', interpolation='nearest')
+    #plt.show()
  
     
 if __name__ == "__main__":
