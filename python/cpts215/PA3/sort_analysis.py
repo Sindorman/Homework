f9dd06d6ff17e70251676d70b7200d780ff1fce8
Sindorman <mikebykhovtsev@gmail.com>
2019-03-03 17:00:18 -0800

    Finished Double Circular Linked List in PA4, might need polish

--- python/cpts215/PA3/sort_analysis.py
+++ python/cpts215/PA3/sort_analysis.py
@@ -15,19 +15,81 @@ import matplotlib.pyplot as plt
 import sys
 
 class Node:
-    def __init__(self):
-        self.data = None
+    def __init__(self, data):
+        self.data = data
         self.prev = None
         self.next = None
+    
+    def __lt__(self, other):
+        return self.data < other.data
 
 class CircularDoublyLinkedList:
     def __init__(self):
         self.head = None
+        self.size = 0
+    
+    def get_node(self, index):
+        current = self.head
+        for i in range(index):
+            current = current.next
+            if current == self.head:
+                return None
+        return current
+    
+    def append(self, new_node):
+        if self.head is None:
+            self.head = new_node
+            new_node.next = new_node
+            new_node.prev = new_node
+            self.size += 1
+        else:
+            end = self.get_node(self.size - 1)
+            end.next = new_node
+            new_node.prev = end
+            new_node.next = self.head
+            self.size += 1
+    
+    def add(self, new_node):
+        self.append(new_node)
+        self.head = new_node
+        self.size += 1
+    
+    def remove(self, node):
+        if self.head.next == self.head:
+            self.head = None
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            if self.head == node:
+                self.head = node.next
+            self.size -= 1
+    
+    def pop(self):
+        end = self.get_node(self.size - 1)
+        end.prev.next = self.head
+        end.prev = None
+        end.next = None
+        self.size -= 1
+        return end
+    
+    def print(self):
+        if self.head is None:
+            return
+        current = self.head 
+        while True:
+            print(current.data, end = ' ')
+            current = current.next
+            if current == self.head:
+                break
+    
+    def __len__(self):
+        return self.size
 
 
 def main():
     '''
     Main funtion definition
+    '''
     '''
     if len(sys.argv) < 3:
         print("Error! Please run script providing name of the files containing data and number of clusters!")
@@ -37,6 +99,16 @@ def main():
     except ValueError:
         print("Error! That is not an number!")
         exit(1)
+    '''
+    cir_list = CircularDoublyLinkedList()
+    for i in range(1, 5001):
+        new_node = Node(i)
+        cir_list.append(new_node)
+    cir_list.print()
+
+    for i in range(2500):
+        cir_list.pop()
+    cir_list.print()
 
     
 if __name__ == "__main__":
