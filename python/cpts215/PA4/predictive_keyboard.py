ebd10f00d7d24c55cf0c0624ac976bf55e5d5d13
Sindorman <mikebykhovtsev@gmail.com>
2019-03-24 15:12:31 -0700

    Foundation of PA4

---
+++ python/cpts215/PA4/predictive_keyboard.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class DictEntry:
+    def __init__(self, word, prob):
+        self.word = word
+        self.prob = prob
+
+    def get_word(self):
+        return self.word
+
+    def get_prob(self):
+        return self.prob
+    
+    def match_pattern(self, pattern):
+        return
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
