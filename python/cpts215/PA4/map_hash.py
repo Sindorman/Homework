69a81b7864f4e39082a89abf97fdbb43bcb74935
Sindorman <mikebykhovtsev@gmail.com>
2019-03-25 18:49:50 -0700

    Finished comments

--- python/cpts215/PA4/map_hash.py
+++ python/cpts215/PA4/map_hash.py
@@ -1,9 +1,27 @@
+'''
+Programmer: Mykhailo Bykhovtsev
+Class: CptS 215, Spring 2019 03/24
+Programming Project #4
+03/24/19
+
+Description: Map class that is an associative map.
+'''
+
 class Node:
+    '''
+    A class representing a node. 
+    Node has key, and value.
+    '''
     def __init__(self, key, value):
         self.key = key
         self.value = value
 
     def __eq__(self, other):
+        '''
+        Method used to compare nodes.
+        Parameter list: Node as node class.
+        return: boolean.
+        '''
         if type(other) is int:
             return False
         if other is None:
@@ -11,18 +29,28 @@ class Node:
         return self.key == other.key
 
     def __str__(self):
+        '''
+        getting string representation of a Node.
+        returns: node as string.
+        '''
         return "{}:{}".format(self.key, self.value)
 
     def __repr__(self):
+        '''
+        used for print()
+        returns: node as string.
+        '''
         return self.__str__()
 
 class Map:
     '''
-    
+    A class representing a Map. 
+    map stores slots as two dimentional array and size as an integer.
     '''
     def __init__(self, size=20011):
         '''
-        
+        Creates new Map instance
+        parameter list: (optional) size of the map to create
         '''
         self.size = size
         self.slots = [None] * self.size
@@ -30,7 +58,7 @@ class Map:
     def put(self, key, item):
         '''
         Place an item in the hash table.
-        Return slot number if successful, -1 otherwise (no available slots, table is full)
+        parameters list: key as anything and item as anything.
         '''
         hashvalue = self.hashfunction(key)
         n = Node(key, item)
@@ -44,7 +72,9 @@ class Map:
         
     def get(self, key):
         '''
-        returns slot position if item in hashtable, -1 otherwise
+        Method of getting value for given key.
+        parameters list: key as anything.
+        returns: item that is stored, -1 if it is not present.
         '''
         hashvalue = self.hashfunction(key)
         n = Node(key, None)
@@ -63,7 +93,8 @@ class Map:
     def remove(self, key):
         '''
         Removes item.
-        Returns slot position if item in hashtable, -1 otherwise
+        parameter list: key as anything.
+        return: item removed or -1 otherwise if not present.
         '''
         hashvalue = self.hashfunction(key)
         n = Node(key, None)
@@ -75,20 +106,20 @@ class Map:
                     self.slots[hashvalue].remove(i)
                     return n
         return -1
-
-    def resize(self):
-        temp = [[None]] * (self.size * (self.size * 0.25))
-        for t in self.slots:
-            index = self.slots.index(t)
-            temp[index] = t
-        self.slots = temp
-        self.size = self.size * (self.size * 0.25)
-
     
     def __len__(self):
+        '''
+        accesing length we use.
+        returns: length as integer.
+        '''
         return self.size
 
     def __contains__(self, key):
+        '''
+        Method to check if a given thing is in the Map.
+        parameters: key as anything.
+        returns: boolean if present.
+        '''
         n = self.get(key)
         if n == -1:
             return False
@@ -96,23 +127,35 @@ class Map:
             return True
 
     def __getitem__(self, key):
+        '''
+        used for accessing items via brackets.
+        parameters list: key as anything.
+        returns: item if present, -1 otherwise.
+        '''
         self.get(key)
 
     def __setitem__(self, key, value):
+        '''
+        Place an item in the hash table. Used for brackets.
+        parameters list: key as anything and item as anything.
+        '''
         self.put(key, value)
 
     def __delitem__(self, key):
+        '''
+        Delete an item. Used for brackets.
+        parameters list: key as anything.
+        '''
         self.remove(key)
 
     def hashfunction(self, item):
         '''
-        Remainder method
+        Function used for hashing.
+        parameters list: key as integer or string
+        returns: hash value as an integer
         '''
         if type(item) is int:
             return item % self.size
-        
-        if type(item) is list:
-            return len(item) % self.size
 
         key = 0
         for x in item:
@@ -120,4 +163,7 @@ class Map:
         return key % self.size
     
     def print(self):
+        '''
+        Printing method.
+        '''
         print(self.slots)
--- python/cpts215/PA4/word_predictor.py
+++ python/cpts215/PA4/word_predictor.py
@@ -1,36 +1,81 @@
 #!/usr/bin/python3
+'''
+Programmer: Mykhailo Bykhovtsev
+Class: CptS 215, Spring 2019 03/24
+Programming Project #4
+03/24/19
+
+Description: This program contains DictEntry and WordPredictor classes that are use for predictive keyboard.
+             This program also has interaction that providie training and building predictive program and interfacing with it. Require N top matches to be inputted as argument.
+'''
 import re
 import sys
 from map_hash import Map
 import timeit
 
 class DictEntry:
+    '''
+    A class representing a Dictionary entry. 
+    Store word as string and probability as float.
+    '''
     def __init__(self, word, prob):
+        '''
+        Creates new entry with given word and probability.
+        Parameter list: word as string and probability as float.
+        '''
         self.word = word
         self.prob = prob
 
     def get_word(self):
+        '''
+        used to get word.
+        returns: word as string
+        '''
         return self.word
 
     def get_prob(self):
+        '''
+        used to get probability.
+        returns: probability as float
+        '''
         return self.prob
     
     def __str__(self):
+        '''
+        used to get output as string.
+        returns: word as string.
+        '''
         return self.word
     
     def __eq__(self, other):
+        '''
+        Class for comparing one entry to another
+        Parameter list: dictionary entry.
+        '''
         if type(other) is int:
             return False
         return self.word == other.word
 
 class WordPredictor:
+    '''
+    A class representing a Word Predictor. 
+    Store words as associative Map and prefixes as associative Map.
+    '''
     def __init__(self, size=20011):
+        '''
+        Creates new Word processor.
+        Parameter list: size of maps to create.
+        '''
         self.word_to_count = Map(size)
         self.prefix_to_entry = Map(size)
         self.total = 0
 
 
     def train(self, training_file):
+        '''
+        Trains on words from a file. Stores all words into word map and how many times given word was seen. Returns error if file was not found.
+        Parameter list: file name as a string.
+        '''
         try:
             f = open(training_file, "r")
             for line in f:
@@ -43,6 +88,10 @@ class WordPredictor:
             return
 
     def train_word(self, word):
+        '''
+        Trains on a single word. Used mostly by train()
+        Parameter list: word as string.
+        '''
         cleanString = (re.sub(r'\W+','', word )).lower()
         if self.word_to_count.get(cleanString ) == -1:
             self.word_to_count.put(cleanString, 1)
@@ -52,6 +101,9 @@ class WordPredictor:
         self.total += 1
 
     def build(self):
+        '''
+        Iterates all words and build their predicitions and prefexis storing in prefix Map. Basic version.
+        '''
         for t in self.word_to_count.slots:
             if t is None:
                 continue
@@ -68,6 +120,9 @@ class WordPredictor:
                         self.prefix_to_entry.put(prefix, dictE)
     
     def build_n(self):
+        '''
+        Iterates all words and build their predicitions and prefexis storing in prefix Map. Advanced version. Stores all word posibilities for a prefix in descending order to get top N.
+        '''
         for t in self.word_to_count.slots:
             if t is None:
                 continue
@@ -91,27 +146,49 @@ class WordPredictor:
                         self.prefix_to_entry.put(prefix, item)
 
     def get_word_count(self, word):
+        '''
+        Method of getting number to times we saw a single word.
+        Parameter list: word as string.
+        returns: value of how many times we saw a word as int.
+        '''
         if self.word_to_count.get(word) == -1:
             return 0
         else:
             return self.word_to_count.get(word)
 
     def get_best(self, prefix):
+        '''
+        Method of getting best single match for a given prefix.
+        Parameter list: prefix as a string.
+        returns: word as a string.
+        '''
         if self.prefix_to_entry.get(prefix) == -1:
             return DictEntry("null", 0)
         else:
             return self.prefix_to_entry.get(prefix)
     
     def get_best_n(self, prefix):
+        '''
+        Method of getting all matches for a given prefix.
+        Parameter list: prefix as a string.
+        returns: list of Dict entries in descending order.
+        '''
         if self.prefix_to_entry.get(prefix) == -1:
             return [DictEntry("null", 0)]
         else:
             return self.prefix_to_entry.get(prefix)
 
     def get_training_count(self):
+        '''
+        Method of accessing total number of words we have.
+        returns: words count as int.
+        '''
         return self.total
 
 def main():
+    '''
+    Definition of main function. Has interface to interact with program.
+    '''
     if len(sys.argv) < 2:
         print("Error! Please run giving N top matches for prefix!")
         exit(1)
