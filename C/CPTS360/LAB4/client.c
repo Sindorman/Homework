945b4d672af71d0bfef39faaf1da5c8027260fda
Mykhailo <mikebykhovtsev@gmail.com>
2020-03-05 19:31:17 -0800

    Temp save

--- C/CPTS360/LAB4/client.c
+++ C/CPTS360/LAB4/client.c
@@ -2,24 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <netdb.h>
 
-#define MAX          256
+
+#define MAX 256
 #define SERVER_HOST "localhost"
-#define SERVER_IP   "127.0.0.1"
-#define SERVER_PORT  1234
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 1234
 
 // Define variables
-struct sockaddr_in  server_addr; 
+struct sockaddr_in server_addr; 
 int sock, r;
+char *curdir;
+
 
 // clinet initialization code
 int client_init()
 {
     printf("======= clinet init ==========\n");
-    
     printf("1 : create a TCP socket\n");
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock<0){
@@ -27,11 +28,13 @@ int client_init()
         exit(1);
     }
 
+
     printf("2 : fill server_addr with server's IP and PORT#\n");
     server_addr.sin_family = AF_INET;
     //server_addr.sin_addr.s_addr = htonl((127<<24) + 1);
     server_addr.sin_addr.s_addr = htons(INADDR_ANY);
     server_addr.sin_port = htons(SERVER_PORT);
+
 
     // Connect to server
     printf("3 : connecting to server ....\n");
@@ -47,28 +50,110 @@ int client_init()
     printf("========= init done ==========\n");
 }
 
+
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+
+char* execute(char *command, char *args)
+{
+    char strip[MAX];
+    memcpy(strip, &command[1], MAX - 1);
+    strip[MAX - 1] = '\0';
+    char *beginning = concat("/bin/", strip);
+    char *passing[] = {beginning, NULL};
+    int status;
+    printf("command: %s", passing[0]);
+    if ( fork() == 0 )
+    {
+        execv(passing[0], passing); // child: call execv with the path and the args
+    }
+    else
+    {
+        wait(& status);
+    }
+}
+
+
+char* process_command(char *args)
+{
+    printf("here");
+    char args_copy[MAX];
+    strcpy(args_copy, args);
+    char *token = strtok(args_copy, " ");
+    char *commands[256];
+    int counter = 0;
+    while(token != NULL)
+    {
+        commands[counter] = token;
+        token = strtok(NULL, " ");
+        counter++;
+    }
+    char *command = commands[0];
+    counter = 1;
+    while(command != NULL || counter != 256)
+    {
+        if(strcmp(command, "lpwd") == 0)
+        {
+            return curdir;
+        }
+        else
+        {
+            execute(command, (char*) args_copy);
+        }
+        command = commands[counter + 1];
+        counter++;
+    }
+}
+
+
 int main(int argc, char *argv[ ])
 {
     int n;
     char line[MAX], ans[MAX];
+    curdir = getcwd(NULL, 0);
+    printf("client: chroot to %s\n", curdir);
+    chroot(curdir);
+
 
     client_init();
+    printf("---------------------------------------------------------\n");
+    printf("Available commands: get put ls cd pwd mkdir rmdir rm \nlcat lls lcd lpwd lmkdir lrmdir lrm\n");
+    printf("---------------------------------------------------------\n");
 
-    printf("********  processing loop  *********\n");
-    while (1){
+
+    printf("******** processing loop *********\n");
+    while (1)
+    {
         printf("input a line : ");
-        bzero(line, MAX);                // zero out line[ ]
-        fgets(line, MAX, stdin);         // get a line (end with \n) from stdin
-        line[strlen(line)-1] = 0;        // kill \n at end
-        if (line[0]==0)                  // exit if NULL line
+        bzero(line, MAX); // zero out line[ ]
+        fgets(line, MAX, stdin); // get a line (end with \n) from stdin
+        line[strlen(line)-1] = 0; // kill \n at end
+        if (line[0]==0) // exit if NULL line
             exit(0);
 
-        // Send ENTIRE line to server
-        n = write(sock, line, MAX);
-        printf("client: wrote n=%d bytes; line=(%s)\n", n, line);
 
-        // Read a line from sock and show it
-        n = read(sock, ans, MAX);
-        printf("client: read  n=%d bytes; echo=(%s)\n", n, ans);
+        // Send ENTIRE line to server
+        if(line[0] == 'l' && line[1] != 's')
+        {
+            char *result = process_command((char*) line);
+            printf("Executing local command: %s\n", result);
+            continue;
+        }
+        else
+        {
+            n = write(sock, line, MAX);
+            printf("client: wrote n=%d bytes; line=(%s)\n", n, line);
+
+
+            // Read a line from sock and show it
+            n = read(sock, ans, MAX);
+            printf("client: read n=%d bytes; echo=(%s)\n", n, ans);
+        }
     }
 }
--- C/CPTS360/LAB4/server.c
+++ C/CPTS360/LAB4/server.c
@@ -2,34 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <netdb.h>
 
-#define  MAX 256
+#define MAX 256
 #define SERVER_HOST "localhost"
-#define SERVER_IP   "127.0.0.1"
+#define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 1234
 
+
 // Define variables:
-struct sockaddr_in  server_addr, client_addr;
+struct sockaddr_in server_addr, client_addr;
 char *curdir;
 int mysock, csock;
 int r, len, n;
 
+
 // Server initialization code:
 int server_init()
 {
-    printf("==================== server init ======================\n");   
+    printf("==================== server init ======================\n"); 
     // get DOT name and IP address of this host
     printf("1 : create a TCP STREAM socket\n");
     mysock = socket(AF_INET, SOCK_STREAM, 0);
-    
     printf("2 : fill server_addr with host IP and PORT# info\n");
     // initialize the server_addr structure
-    server_addr.sin_family = AF_INET;                  // for TCP/IP
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);   // THIS HOST IP address  
-    server_addr.sin_port = htons(SERVER_PORT);         // port number
+    server_addr.sin_family = AF_INET; // for TCP/IP
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // THIS HOST IP address 
+    server_addr.sin_port = htons(SERVER_PORT); // port number
 
     printf("3 : bind socket to server address\n");
     // bind syscall: bind the socket to server_addr info
@@ -39,11 +39,11 @@ int server_init()
         exit(3);
     }
     printf("host = %s port = %d\n", SERVER_HOST, SERVER_PORT);
-    
     printf("4 : server is listening ....\n");
     listen(mysock, 5); // listen queue length = 5
     printf("===================== init done =======================\n");
 }
+
 
 char* concat(const char *s1, const char *s2)
 {
@@ -53,45 +53,30 @@ char* concat(const char *s1, const char *s2)
     return result;
 }
 
-char* execute(char *args)
+
+char* execute(char *command, char *args)
 {
-    char line_copy[MAX];
-    strcpy(line_copy, args);
-    printf("here");
-    char *token = strtok(line_copy, " ");
     // printf("here");
-    char *beginning = concat("/bin/", token);
+    char *beginning = concat("/bin/", command);
     char *passing[] = {beginning, NULL};
-    // printf("here");
-    // char *command[256];
-    // printf("here");
-    // command[0] = beginning;
-    // printf("here");
-    // int counter = 1;
-    // while(token != NULL)
-    // {
-    //     command[counter] = strtok(NULL, " ");
-    //     counter++;
-    // }
-    // command[counter + 1] = NULL;
     int pipefd[2];
     pipe(pipefd);
     int status;
-    printf("command: %s", passing[0]);
     if ( fork() == 0 )
     {
         close(pipefd[0]);
 
-        dup2(pipefd[1], 1);  // send stdout to the pipe
-        dup2(pipefd[1], 2);  // send stderr to the pipe
+
+        dup2(pipefd[1], 1); // send stdout to the pipe
+        dup2(pipefd[1], 2); // send stderr to the pipe
         close(pipefd[1]);
         execv(passing[0], passing); // child: call execv with the path and the args
-        //execv(command[0], command); // child: call execv with the path and the args
     }
     else
     {
         char buffer[1024];
-        close(pipefd[1]);  // close the write end of the pipe in the parent
+        close(pipefd[1]); // close the write end of the pipe in the parent
+
 
         while (read(pipefd[0], buffer, sizeof(buffer)) != 0)
         {
@@ -101,17 +86,47 @@ char* execute(char *args)
 }
 
 
+char* process_command(char *args)
+{
+    char args_copy[MAX];
+    strcpy(args_copy, args);
+    char *token = strtok(args_copy, " ");
+    char *commands[256];
+    int counter = 0;
+    while(token != NULL)
+    {
+        commands[counter] = token;
+        token = strtok(NULL, " ");
+        counter++;
+    }
+    char *command = commands[0];
+    counter = 1;
+    while(command != NULL || counter != 256)
+    {
+        if(strcmp(command, "pwd") == 0)
+        {
+            return curdir;
+        }
+        else
+        {
+            return execute(command, (char*)args_copy);
+        }
+        command = commands[counter + 1];
+        counter++;
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
     char line[MAX];
     server_init();
     curdir = getcwd(NULL, 0);
-    printf("server : chroot to %s\n", curdir);
+    printf("server: chroot to %s\n", curdir);
     chroot(curdir);
-    
-    while(1){    // Try to accept a client request
+    while(1){ // Try to accept a client request
         printf("server: accepting new connection ....\n"); 
+
 
         // Try to accept a client connection as descriptor csock
         len = sizeof(client_addr);
@@ -122,30 +137,34 @@ int main(int argc, char *argv[])
         }
         printf("server: accepted a client connection from\n");
         printf("-----------------------------------------------\n");
-        printf("Client: IP=%s  port=%d\n", "127.0.0.1",
-                                ntohs(client_addr.sin_port));
+        printf("Client: IP=%s port=%d\n", "127.0.0.1",
+        ntohs(client_addr.sin_port));
         printf("-----------------------------------------------\n");
 
+
         // Processing loop: csock <-- data --> client
-        while(1){
+        while(1)
+        {
             n = read(csock, line, MAX);
-            if (n==0){
+            if (n==0)
+            {
                 printf("server: client died, server loops\n");
                 close(csock);
                 break;
             }
             // show the line string
-            printf("server: read  n=%d bytes; line=[%s]\n", n, line);
-            char *result = execute((char*) line);
-            printf("ans %s", result);
+            printf("server: read n=%d bytes; line=[%s]\n", n, line);
+            char *result = process_command((char*) line);
+
 
             // process the line from clinet
             strcat(line, " ECHO");
             // send the echo line to client 
             n = write(csock, result, MAX);
 
+
             printf("server: wrote n=%d bytes; ECHO=[%s]\n", n, line);
             printf("server: ready for next request\n");
+            }
         }
-    }
 }
