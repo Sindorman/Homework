934efb31b81d25cadf048ac3990a3d7fa22d98e7
Sindorman <mikebykhovtsev@gmail.com>
2020-03-12 10:19:12 -0700

    extra

--- C/CPTS360/project/cd_ls_pwd.c
+++ C/CPTS360/project/cd_ls_pwd.c
@@ -1,14 +1,4 @@
 /************* cd_ls_pwd.c file **************/
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <fcntl.h>
-// #include <ext2fs/ext2_fs.h>
-// #include <string.h>
-// #include <libgen.h>
-// #include <sys/stat.h>
-// #include <time.h>
-
-// #include "type.h"
 
 char *t1 = "xwrxwrxwr-------";
 char *t2 = "----------------";
